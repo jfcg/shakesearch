3c8b2fe8c3e7944d775dd97f5ec31c24ec8f9fd9
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2020-12-16 23:04:17 +0300

    make handleSearch a regular HandleFunc
    by making searcher global since we have only one.

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ import (
 	"os"
 )
 
+var searcher Searcher
+
 func main() {
-	searcher := Searcher{}
 	err := searcher.Load("completeworks.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -21,7 +22,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/search", handleSearch(searcher))
+	http.HandleFunc("/search", handleSearch)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -39,26 +40,24 @@ type Searcher struct {
 	SuffixArray *suffixarray.Index
 }
 
-func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		query, ok := r.URL.Query()["q"]
-		if !ok || len(query[0]) < 1 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("missing search query in URL params"))
-			return
-		}
-		results := searcher.Search(query[0])
-		buf := &bytes.Buffer{}
-		enc := json.NewEncoder(buf)
-		err := enc.Encode(results)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("encoding failure"))
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(buf.Bytes())
+func handleSearch(w http.ResponseWriter, r *http.Request) {
+	query, ok := r.URL.Query()["q"]
+	if !ok || len(query[0]) < 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("missing search query in URL params"))
+		return
 	}
+	results := searcher.Search(query[0])
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	err := enc.Encode(results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("encoding failure"))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf.Bytes())
 }
 
 func (s *Searcher) Load(filename string) error {
